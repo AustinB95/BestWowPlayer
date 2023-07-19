524e82c4b36dd7e1b32072c4d4d560690dd47842
AustinB95 <138809694+AustinB95@users.noreply.github.com>
2023-07-19 15:12:32 -0700

    a

--- matchlove.js
+++ matchlove.js
@@ -1,84 +1,53 @@
-var n = Math.random() * 100 + 1;
+// var n = Math.random() * 100 + 1;
 
-n = Math.floor(n);
-alert("Would you like to see how compatible you and your crush are?");
-var name = prompt("What is your name?").trim().toLowerCase();
-var crush = prompt("What is your Crushes name").trim().toLowerCase();
-// alert(name + " and " + crush +  " are a " + n + "% match!" )
+// n = Math.floor(n);
+// alert("Would you like to see how compatible you and your crush are?");
+// var yourName = prompt("What is your name?").trim().toLowerCase();
+// var crush = prompt("What is your Crushes name").trim().toLowerCase();
+// // alert(name + " and " + crush +  " are a " + n + "% match!" )
 
-name.toLowerCase;
-crush.toLowerCase;
+// yourName.toLowerCase;
+// crush.toLowerCase;
+
+// let a = 'austin'
+// let k = 'kelly'
 
 
 
-    if ((name === 'kelly' && crush === 'austin') || (name === 'austin' && crush === 'kelly')) {
-    alert("Doesn't matter what the % is, You guys are meant to be!");
-            } else if((name === 'kelly' && crush != 'austin') || (name === 'austin' && crush != 'kelly')
-            || (name != 'kelly' && crush === 'austin') || (name != 'austin' && crush === 'kelly')){
-            alert("Not Compatible")}
-                else if (n < 50){
-                alert("Sorry! Your score is " + n)}
-                        else {
-                        alert("Congrats! Your score is " + n)};
-
-                        //  } else if (n < 50 && name != 'kelly' && name != 'austin' && crush != 'kelly' && crush != 'austin'){
-                        //    alert("Sorry! Your score is " + n)}
-                        //          else if (n > 50 && name != 'kelly' && name != 'austin' && crush != 'kelly' && crush != 'austin'){
-                        //            alert("Congrats! Your score is " + n)};
-                            
+//     // if ((yourName === k && crush === a) || (yourName === a && crush === k)) {
+//     // alert("Doesn't matter what the % is, You guys are meant to be!");
+//     //         } else if((yourName === k && crush != a) || (yourName === a && crush != k)
+//     //         || (yourName != k && crush === a) || (yourName != a && crush === k)){
+//     //         alert("Not Compatible")}
+//     //             else if (n < 50){
+//     //             alert(`Sorry! ${yourName} and ${crush}'s compatibility is only ${n}%`)}
+//     //                     else {
+//     //                     alert(`Congrats! ${yourName} and ${crush}'s compatibility is ${n}%`)};
 
 
 
 
-      
-     
-
-
-
-
-
-
-
-
-// if (name === 'kelly' && crush === 'austin'){
+//      if (yourName === crush){
+//         alert(`Yes, you should always love yourself!`)}
+//     else if ((yourName === k && crush === a) || (yourName === a && crush === k)) {
 //     alert("Doesn't matter what the % is, You guys are meant to be!");
-// }
+//             } else if((yourName === k && crush != a) || (yourName === a && crush != k)
+//             || (yourName != k && crush === a) || (yourName != a && crush === k)){
+//             alert("Not Compatible")}
 
-// if (name === 'austin' && crush === 'kelly'){
-//     alert("Doesn't matter what the % is, You guys are meant to be!");
-// }
-
-
-
-// if (name === 'kelly' && crush != 'austin'){
-//     alert("Sorry, Kelly does not belong with anyone else.");
-// }
-
-// if (name != 'austin' && crush === 'kelly'){
-//     alert("Sorry, Kelly does not belong with anyone else.");
-// }
+//                 else if (n < 50){
+//                 alert(`Sorry! ${yourName} and ${crush}'s compatibility is only ${n}%`)}
+//                         else {
+//                         alert(`Congrats! ${yourName} and ${crush}'s compatibility is ${n}%`)};
 
 
 
 
 
 
-// if (n < 50 && name != 'kelly' && name != 'austin' && crush != 'kelly' && crush != 'austin'){
-//         alert("Sorry! Your score is " + n);}
-
-// if (n > 50 && name != 'kelly' && name != 'austin' && crush != 'kelly' && crush != 'austin'){
-//         alert("Congrats! Your score is " + n);}
 
 
 
-
-
-// if (n < 50 && name != 'kelly' + 'austin' && crush != 'kelly' + 'austin'){
-//     alert("Oh No! It looks like " + name +  " and " + crush + " Are not a very good Match at " + n + "%!");
-// }
-
-// if (n > 50 && name != 'kelly' + 'austin' && crush != 'kelly' + 'austin'){
-//     alert("Contrats! It looks like " + name +  " and " + crush + " Are a Great Match at " + n + "%!" );}
 
 
 
